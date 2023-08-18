bfaea043ac194902de3559e1d4d305d5fcab44a3
Abhishekyadav2096 <104411173+Abhishekyadav2096@users.noreply.github.com>
2023-08-18 13:18:36 +0530

    Update index.js
    changes in code

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.send("ABHISHEK YADAV DEVELOPER");
+  return res.send("ABHISHEK YADAV CICD PIPELINE");
 });
 
 app.listen(5001, () => {
