5d362a3daf5df5da30edc5caded57c75b8cc402a
Abhishek Yadav <abhishek.yadav@harnstech.com>
2023-08-18 13:48:10 +0530

    changed index.js file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.send("ABHISHEK YADAV CICD PIPELINE");
+  return res.send("ABHISHEK YADAV");
 });
 
 app.listen(5001, () => {
